c845b891525a7bd2a156d765ccac17c605827ca0
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-12-20 18:36:22 +0800

    C Sharp練習

---
+++ Unity_2DGame/Assets/Scripts/LearnCSharp.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour
+{
+    public int hp = 100;
+    private float speed = 1.5f;
+    public string skill = "星爆氣流斬";
+    private bool mission = false;
+    public Color skin = Color.black;
+    private Vector3 posStart = new Vector3(30,1,50);
+    public 
+}
