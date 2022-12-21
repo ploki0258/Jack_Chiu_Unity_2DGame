e5c6772dccdf161909a0d1f77e044fb96e390c23
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-12-21 17:58:42 +0800

    C Sharp練習

--- Unity_2DGame/Assets/Scripts/LearnCSharp.cs
+++ Unity_2DGame/Assets/Scripts/LearnCSharp.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 public class LearnCSharp : MonoBehaviour
 {
     public int hp = 100;
-    private float speed = 1.5f;
     public string skill = "星爆氣流斬";
-    private bool mission = false;
     public Color skin = Color.black;
-    private Vector3 posStart = new Vector3(30,1,50);
-    public 
+    public GameObject trap;
+    private float speed = 1.5f;
+    private bool mission = false;
+    private Vector3 posStart = new Vector3(30, 1, 50);
+
+    /// <summary>
+    /// 移動方法
+    /// </summary>
+    public void Move()
+    {
+
+    }
+
+    /// <summary>
+    /// 受傷方法
+    /// </summary>
+    /// <param name="getDamage">接受的傷害值</param>
+    private void Hit(float getDamage)
+    {
+
+    }
+
+    /// <summary>
+    /// 跳躍方法
+    /// </summary>
+    /// <param name="height">跳躍高度</param>
+    /// <param name="aniName">動畫名稱</param>
+    public void jump(float height,string aniName)
+    {
+
+    }
 }
